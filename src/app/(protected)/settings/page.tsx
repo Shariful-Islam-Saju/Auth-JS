66a1abe9c48ef82b1bf52158ba4cccd24169e589
Shariful Islam <sajukhan12905@gmail.com>
2025-03-08 00:39:03 +0600

    add sign out

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,10 +1,32 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Settings = async () => {
   const session = await auth();
   console.log(session);
-  return <div>Settings</div>;
+  return (
+    <div className="flex flex-col justify-center items-center h-full ">
+      <p className="text-white font-bold text-lg my-5 max-w-[800px]">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium
+        nostrum et, temporibus reiciendis iusto maiores, esse, fuga facilis a
+        dolorum saepe aspernatur voluptatum? Minus, neque incidunt. Distinctio,
+        ex? Quos quam fugit ut consequuntur minima, enim nihil iure magni minus
+        voluptatum vel error deserunt, provident laudantium voluptates nostrum
+        consequatur obcaecati a.
+      </p>
+      <form
+        action={async () => {
+          'use server'
+          await signOut();
+        }}
+      >
+        <Button variant={"outline"} type="submit">
+          Log Out
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default Settings;
