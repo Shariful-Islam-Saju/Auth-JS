467cb2b0fee5c1f767aa9659fb49b4173ef6440e
Shariful Islam <sajukhan12905@gmail.com>
2025-03-08 19:58:52 +0600

    use google and github

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,11 +1,21 @@
 import bcrypt from "bcryptjs";
 import CredentialsProvider from "next-auth/providers/credentials";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import type { NextAuthConfig } from "next-auth";
 import { LoginSchemas } from "./schemas";
 import { getUserByEmail } from "./lib/user";
 
 export default {
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
     CredentialsProvider({
       async authorize(credentials) {
         try {
--- src/components/auth/Social.tsx
+++ src/components/auth/Social.tsx
@@ -2,15 +2,24 @@ import React from "react";
 import { FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { Button } from "../ui/button";
+import { signIn } from "next-auth/react";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 const Social = () => {
+  function handleSignIn(provider: "google" | "github") {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  }
   return (
     <div className="flex items-center w-full gap-x-2">
       <Button
         className="w-full"
         size={"lg"}
         variant="outline"
-        onClick={() => {}}
+        onClick={() => {
+          handleSignIn("google");
+        }}
       >
         <FcGoogle className="h-5 w-5" />
       </Button>{" "}
@@ -18,7 +27,9 @@ const Social = () => {
         className="w-full"
         size={"lg"}
         variant="outline"
-        onClick={() => {}}
+        onClick={() => {
+          handleSignIn("github");
+        }}
       >
         <FaGithub className="h-5 w-5" />
       </Button>
